38c73a9fd48fc754701d5bee73705a3ad86f1f26
optshj <optshj@gmail.com>
2026-06-07 14:30:29 +0900

    🎉 [Init] 프로젝트 초기세팅 (ESlint + FSD + 템플릿)

---
+++ src/views/home/ui/HomePage.tsx
@@ -0,0 +1,8 @@
+export const HomePage = () => {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1 className="mb-4 text-4xl font-bold">Welcome to the Home Page</h1>
+      <p className="mb-8 text-lg text-gray-600">This is the main landing page of our application.</p>
+    </div>
+  )
+}
