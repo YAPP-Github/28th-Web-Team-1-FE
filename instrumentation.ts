f76ccbf2e2d47d6a62a4adff1bfc3677d11452ac
leeleeleeleejun <wnstjr6293@gmail.com>
2026-06-21 19:53:49 +0900

    ⚙️[Chore] MSW 브라우저/서버 초기화 설정 (#14) - instrumentation에서 서버 MSW 초기화 연결 - app provider에서 브라우저 MSW 초기화 컴포넌트 실행 - mock handler 및 data 디렉터리 기본 구조 추가

--- instrumentation.ts
+++ instrumentation.ts
@@ -3,6 +3,9 @@ import * as Sentry from '@sentry/nextjs'
 export const register = async () => {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     await import('./sentry.server.config')
+
+    const { initServerMSW } = await import('@shared/mocks')
+    await initServerMSW()
   }
 
   if (process.env.NEXT_RUNTIME === 'edge') {
