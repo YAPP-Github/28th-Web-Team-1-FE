54488ca0e795dec6ada05fc2f1b44026b0b6c126
leeleeleeleejun <wnstjr6293@gmail.com>
2026-07-14 22:41:58 +0900

    ✨[Feat] useExperienceList 훅을 useSuspenseInfiniteQuery로 변경하여 워크스페이스 ID 필수화 및 데이터 반환 방식 수정 (#61)

--- src/entities/experience/model/experience.queries.ts
+++ src/entities/experience/model/experience.queries.ts
@@ -1,12 +1,11 @@
 'use client'
-import { useInfiniteQuery } from '@tanstack/react-query'
+import { useSuspenseInfiniteQuery } from '@tanstack/react-query'
 import { experienceQueries } from './experience.keys'
 
-export const useExperienceList = (workspaceId?: string) => {
-  const { data, ...rest } = useInfiniteQuery({
-    ...experienceQueries.list(workspaceId ?? ''),
-    enabled: Boolean(workspaceId),
+export const useExperienceList = (workspaceId: string) => {
+  const { data, ...rest } = useSuspenseInfiniteQuery({
+    ...experienceQueries.list(workspaceId),
     select: (data) => data.pages.flatMap((page) => page.experiences.experiences)
   })
-  return { experiences: data ?? [], ...rest }
+  return { experiences: data, ...rest }
 }
