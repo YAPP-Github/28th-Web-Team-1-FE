8a1e05d3cb92b3fd5681fa44eab8a62b00b0545c
optshj <optshj@gmail.com>
2026-08-01 14:52:26 +0900

    🐞 [Fix] 로고 사이즈 수정 (#87)

--- src/shared/icon/Logo.tsx
+++ src/shared/icon/Logo.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 
 export const Logo = () => {
   return (
-    <Link href="/" className="font-elms rounded-xs px-1 py-0.5 text-2xl font-bold tracking-[-0.02em]">
-      Scoop
+    <Link href="/" className="font-elms rounded-xs px-1 py-0.5 text-xl font-bold tracking-[-0.02em]">
+      SCOOP
     </Link>
   )
 }
